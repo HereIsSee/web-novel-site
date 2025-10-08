f32368d51c53f286dd393d82efabba3cdd51b2ba
HereIsSee <biramantvydas@gmail.com>
2025-10-09 00:37:11 +0300

    Add front-end react, with proxy listening to back-end

---
+++ front-end/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    proxy: {
+      '/api': {
+        target: 'htpp://localhost:5107',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+})
