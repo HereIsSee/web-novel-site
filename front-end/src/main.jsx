b8bde2fc3554243853439ebfa9c15e7742055357
HereIsSee <biramantvydas@gmail.com>
2025-10-19 01:24:03 +0300

    Add basic laybout for Chapter component

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -12,9 +12,10 @@ import Home from './routes/Home'
 import Novel from './routes/Novel'
 import Profile from './routes/Profile'
 import ReadLater from './routes/ReadLater'
-import Search from './routes/ReadLater'
+import Search from './routes/Search'
 import Login from './routes/Login'
 import Register from './routes/Register'
+import Chapter from './routes/Chapter'
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -28,6 +29,7 @@ const router = createBrowserRouter([
   { path: "/novels/:id/:novelSlug", element: <Novel /> },
   { path: "/login", element: <Login />},
   { path: "/register", element: <Register /> },
+  { path: "/novels/:id/:novelSlug/chapters/:id/:chapterSlug", element: <Chapter />}
 ]);
 
 createRoot(document.getElementById('root')).render(
