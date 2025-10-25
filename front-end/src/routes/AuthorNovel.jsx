4fc96db96e7977f3ebb27eb4303d8c3b222f1db2
HereIsSee <biramantvydas@gmail.com>
2025-10-25 18:51:30 +0300

    Update AuthorNovel route to show updated novel statistics

--- front-end/src/routes/AuthorNovel.jsx
+++ front-end/src/routes/AuthorNovel.jsx
@@ -1,41 +1,35 @@
-import { Link, useNavigate, Navigate, useParams } from "react-router-dom";
-import { EditorContent, useEditor } from "@tiptap/react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useToast } from "../context/useToast";
-import { uploadCoverTemp, createNovel, getNovel } from "../api/novel";
-import { getTags } from "../api/tags";
-import StarterKit from "@tiptap/starter-kit";
-import DOMPurify from "dompurify";
+import { getNovel } from "../api/novel";
+import { getNovelStats } from "../api/novelInteractions";
 import AuthorDashboardLayout from "../components/AuthorDashboardLayout";
-import TextEditor from "../components/FormFields/TextEditor/TextEditor";
-import checkImageRatio from "../helpers/checkImageRatio";
-import InputField from "../components/FormFields/InputField";
-import Button from "../components/FormFields/Button";
-import DropDown from "../components/FormFields/DropDown";
-import DropDownListSelection from "../components/FormFields/DropDownListSelection";
 
 import NovelHeader from "../components/Novel/NovelHeader";
 import NovelInfo from "../components/Novel/NovelInfo";
 import NovelStatistics from "../components/Novel/NovelStatistics";
-import NovelActionButtons from "../components/Novel/NovelActionButtons";
 import NovelTableOfContents from "../components/Novel/NovelTableOfContents";
-
-const novelStatusArray = { Draft: 0, Published: 1, Hidden: 3 };
 
 const AuthorNovel = () => {
   const { userId, novelId } = useParams();
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [novelStats, setNovelStats] = useState({});
   const [novel, setNovel] = useState({});
+
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    const novelData = async () => {
+    const fetchData = async () => {
       try {
-        const response = await getNovel(novelId);
-        setNovel(response);
+        const [novelData, novelStatsData] = await Promise.all([
+          getNovel(novelId),
+          getNovelStats(novelId),
+        ]);
+
+        setNovel(novelData);
+        setNovelStats(novelStatsData);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -43,7 +37,7 @@ const AuthorNovel = () => {
       }
     };
 
-    novelData();
+    fetchData();
   }, [novelId]);
 
   return (
@@ -70,7 +64,7 @@ const AuthorNovel = () => {
 
           <NovelInfo tags={novel.tags} synopsis={novel.synopsis} />
 
-          <NovelStatistics />
+          <NovelStatistics novelStats={novelStats} />
 
           <NovelTableOfContents />
         </div>
